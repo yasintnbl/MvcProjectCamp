d33771f0e8eb9bead381d679eef4c1ab2874a061
yasintnbl <yasintnbl@gmail.com>
2021-06-27 13:35:21 +0300

    3. Sertifika Görevi

--- BusinessLayer/Abstract/IMessageService.cs
+++ BusinessLayer/Abstract/IMessageService.cs
@@ -10,8 +10,8 @@ namespace BusinessLayer.Abstract
    public interface IMessageService
     {
         List<Message> GetAll();
-        List<Message> GetListInbox();
-        List<Message> GetListSendbox(); 
+        List<Message> GetListInbox(string p);
+        List<Message> GetListSendbox(string p); 
         List<Message> GetAllUnRead();
         List<Message> IsDraft();
         void MessageAdd(Message message);
